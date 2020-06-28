788846b6821d850dab8d0e2a4fda969dd1946d1e
Ryan Mulligan <ryan@ryantm.com>
2020-06-28 15:51:13 -0700

    update home.nix based on feedback from Rycee

--- home.nix
+++ home.nix
@@ -2,8 +2,38 @@
 
 {
 
-  # Configuration options are documented at
+  # The home-manager manual is at:
+  #
+  #   https://rycee.gitlab.io/home-manager/release-notes.html
+  #
+  # Configuration options are documented at:
+  #
   #   https://rycee.gitlab.io/home-manager/options.html
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  #
+  # You need to change these to match your username and home directory
+  # path:
+  home.username = "$USER";
+  home.homeDirectory = "$HOME";
+
+  # If you use non-standard XDG locations, set these options to the
+  # appropriate paths:
+  #
+  # xdg.cacheHome
+  # xdg.configHome
+  # xdg.dataHome
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "20.09";
 
   # Since we do not install home-manager, you need to let home-manager
   # manage your shell, otherwise it will not be able to add its hooks
